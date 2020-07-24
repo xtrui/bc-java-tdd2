708efe060965e9423487419239686553c29d5ed3
Ray <ray.wen@oocl.com>
2020-07-24 17:36:31 +0800

    [Ray] test: modify should_return_false_when_isValid_given_1123 test.

--- src/test/java/example/ValidToolTest.java
+++ src/test/java/example/ValidToolTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ValidToolTest {
@@ -18,7 +19,10 @@ public class ValidToolTest {
 
     @Test
     void should_return_false_when_isValid_given_1123() {
-
-        assertEquals(false, ValidTool.isValid("1123"));
+        assertFalse(ValidTool.isValid("1123"));
+        assertFalse(ValidTool.isValid("11234"));
+        assertFalse(ValidTool.isValid("142A"));
+        assertTrue(ValidTool.isValid("1356"));
     }
+
 }
